aabe79f3a4a3b3cfe701c5d31f760cb2284d2f69
Andrew J. Davis <github@azotus.co.uk>
2022-08-21 21:02:55 -0300

    general tidy up

--- adFns.py
+++ adFns.py
@@ -1,4 +1,47 @@
 #!/usr/bin/python3
+import os
+import sys
+
+debugging = not True
+from adText import print_justified
+
+# ##
+#
+#   A
+#  A
+#  A
+#  AAAA
+#  A
+#  A
+#  A
+#
+# ##
+
+## This allows this file to display help, but only if executed directly:
+if os.path.basename(__file__) in sys.argv[0]:
+  print_justified("_adFns.py Help Information_")
+  print ("")
+  print ("General Purpose Functions that I could not find a better home for:")
+  print (". AddDevPaths()")
+  print (". GetValue()")
+  print (". is_float()")
+  print (". is_int()")
+  print ("")
+  print ("Planned Improvements:")
+  print ("- ")
+  sys.exit(0)
+
+def AddDevPaths():
+  fldrList = []
+  fldrList.append("/mnt/shr/{Drvs}/ref/Python")
+  fldrList.append("/home/andrew/shr/{Drvs}/ref/Python")
+
+  for fldrPath in fldrList:
+    if os.path.exists(fldrPath):
+      if len(os.listdir(fldrPath)) > 0:
+        sys.path.insert(0, fldrPath)
+        break
+  return
 
 def GetValue(s):
   debugging = not True
